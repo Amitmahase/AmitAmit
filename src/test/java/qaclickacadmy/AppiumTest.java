bd0a566971e9a720149c4edf20723309c692b193
Amit <amitmahaseth1992@gmail.com>
2021-10-25 07:58:03 +0100

    commitfirst

---
+++ src/test/java/qaclickacadmy/AppiumTest.java
@@ -0,0 +1,24 @@
+package qaclickacadmy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void nativeappAndroid()
+	
+	{
+		
+		System.out.println("Native App andriod");
+	}
+
+	@Test
+	public void IosApp()
+	
+	{
+		
+		System.out.println("IOS Apps");
+	}
+
+	
+}
---
+++ src/test/java/qaclickacadmy/SeleniumTest.java
@@ -0,0 +1,26 @@
+package qaclickacadmy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	
+	public void Browser()
+	{
+		System.out.println("Browser test");
+	
+		
+	}
+	
+@Test
+	
+	public void ElementUi()
+	{
+		System.out.println("Element test");
+	
+		
+	}
+
+
+}
